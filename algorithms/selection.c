aaeb3682a2965624430865ada7ac0f1ff603d188
Kheldin <kheldin.acherchour@gmail.com>
2026-01-06 11:46:26 +0100

    normed selection

--- algorithms/selection.c
+++ algorithms/selection.c
@@ -6,23 +6,23 @@
 /*   By: kacherch <kacherch@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 20:30:47 by anrogard          #+#    #+#             */
-/*   Updated: 2026/01/06 11:43:55 by kacherch         ###   ########.fr       */
+/*   Updated: 2026/01/06 11:46:10 by kacherch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 #include "../libft/includes/libft.h"
 
-static void    final_push(t_list **stack_a, t_list **stack_b)
+static void	final_push(t_list **stack_a, t_list **stack_b)
 {
-    int	stack_size;
+	int	stack_size;
 
 	stack_size = ft_lstsize(*stack_b);
-    while (stack_size > 0)
-    {
-        push(stack_a, stack_b, 'a');
-        stack_size--;
-    }
+	while (stack_size > 0)
+	{
+		push(stack_a, stack_b, 'a');
+		stack_size--;
+	}
 }
 
 static void	rotate_direction(t_list **stack_a, int stack_size, int pos_temp)
@@ -37,7 +37,7 @@ static void	rotate_direction(t_list **stack_a, int stack_size, int pos_temp)
 	}
 	else
 	{
-		while(diff > 0)
+		while (diff > 0)
 		{
 			reverse_rotate(stack_a, 'a');
 			diff--;
@@ -56,15 +56,15 @@ static int	find_pos(t_list **stack_a)
 	current = *stack_a;
 	temp = current;
 	while (current)
+	{
+		if ((current)->content < temp->content)
 		{
-			if ((current)->content < temp->content)
-			{
-				temp = current;
-				pos_temp = position;
-			}
-			current = current->next;
-			position++;
-        }
+			temp = current;
+			pos_temp = position;
+		}
+		current = current->next;
+		position++;
+	}
 	return (pos_temp);
 }
 
@@ -80,6 +80,6 @@ void	selection_sort(t_list **stack_a, t_list **stack_b)
 		rotate_direction(stack_a, stack_size, pos_temp);
 		push(stack_a, stack_b, 'b');
 		stack_size--;
-    }
+	}
 	final_push(stack_a, stack_b);
 }
